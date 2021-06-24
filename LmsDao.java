cbd5f9acbd74549ba1338448e70b6d8285482d50
sonampatil2400 <85982582+sonampatil2400@users.noreply.github.com>
2021-06-24 20:32:02 +0530

    Updates 24th June

--- LmsDao.java
+++ LmsDao.java
@@ -1,13 +1,14 @@
 package lms.dao;
 
 import lms.bean.Book;
+import lms.bean.IssueDetails;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class LmsDao {
-
-
     public static Connection con;
     ResultSet rs, rs1;
     static Statement st, st1;
@@ -63,12 +64,10 @@ public class LmsDao {
             System.out.println("update handled: " + e);
             e.printStackTrace();
         }
-
         return rowsupdated;
     }
 
     public int deleteBooks(Book book) {
-
         int rowsupdated = 0;
         try (PreparedStatement pst = con.prepareStatement("delete from books where bookid=?")) {
             pst.setInt(1, book.getBookId());
@@ -77,22 +76,126 @@ public class LmsDao {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
         return rowsupdated;
     }
 
-    public int searchBooks(Book book) {
-
-        int rowsupdated = 0;
+    public Book searchBooks(int bookid) {
         try (PreparedStatement pst = con.prepareStatement("select * from books where bookid=?")) {
-            pst.setInt(1, book.getBookId());
+            pst.setInt(1, bookid);
+            rs = pst.executeQuery();
+            System.out.println(" BookId " + "  " + " Book Name " + "  " + " Publisher " + "  " + " Quantity " + "  " + " Author");
+            Book book = new Book();
 
+            while (rs.next()) {
+                int bId = rs.getInt("bookid");
+                String bName = rs.getString(2);
+                String bPublisher = rs.getString(3);
+                int bQuantity = rs.getInt(4);
+                String bAuthor = rs.getString(5);
+
+                book.setBookId(bId);
+                book.setBookName(bName);
+                book.setPublisher(bPublisher);
+                book.setQuantity(bQuantity);
+                book.setAuthor(bAuthor);
+                return book;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void returnBooks() {
+
+    }
+
+    public ArrayList<IssueDetails> pendingIssueRequests() {
+        try (PreparedStatement pst = con.prepareStatement(" select bookid, userid from issuedetails where issuedate = null")) {
+            rs = pst.executeQuery();
+
+
+            ArrayList<IssueDetails> issueDetailsList = new ArrayList<>();
+
+            while (rs.next()) {
+                int bid = rs.getInt("bookid");
+                int userid = rs.getInt("userid");
+
+                IssueDetails issueDetails = new IssueDetails();
+                issueDetails.setBookId(bid);
+                issueDetails.setUserId(userid);
+
+                issueDetailsList.add(issueDetails);
+            }
+            return issueDetailsList;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void issueBook(int bookId, int userId) {
+        int rowsupdated = 0;
+        try (PreparedStatement pst = con.prepareStatement(" update issuedetails set issuedate=CURDATE() where bookid=? and userid=?")) {
+            pst.setInt(1, bookId);
+            pst.setInt(2, userId);
             rowsupdated = pst.executeUpdate();
+            System.out.println(" Book with book Id  " + bookId + "   has been issued to User  " + userId);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void orderBooks(int userid, int bid) {
+        int rowsaffected = 0;
+        try (PreparedStatement pst = con.prepareStatement(" insert into issuedetails(userid, bookid) values(?,?)")) {
+
+            pst.setInt(1, userid);
+            pst.setInt(2, bid);
+            rowsaffected = pst.executeUpdate();
+            System.out.println("Order for " + bid + " has been placed for user " + userid);
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        return rowsupdated;
+
+    }
+
+    public List<Book> viewAllBooks() {
+        try (PreparedStatement pst = con.prepareStatement("select * from books")) {
+            rs = pst.executeQuery();
+            System.out.println(" BookId " + "  " + " Book Name " + "  " + " Publisher " + "  " + " Quantity " + "  " + " Author");
+
+            ArrayList<Book> bookList = new ArrayList<>();
+
+            while (rs.next()) {
+                int bId = rs.getInt(1);
+                String bName = rs.getString(2);
+                String bPublisher = rs.getString(3);
+                int bQuantity = rs.getInt(4);
+                String bAuthor = rs.getString(5);
+
+                Book book = new Book();
+                book.setBookId(bId);
+                book.setBookName(bName);
+                book.setPublisher(bPublisher);
+                book.setQuantity(bQuantity);
+                book.setAuthor(bAuthor);
+
+                bookList.add(book);
+
+            }
+            return bookList;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void searchStudentBooks() {
+
     }
 
     public void closeResources() {
--- LmsView.java
+++ LmsView.java
@@ -1,8 +1,12 @@
 package lms.view;
 
 import lms.bean.Book;
+import lms.bean.IssueDetails;
 import lms.dao.LmsDao;
 
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class LmsView {
@@ -19,6 +23,7 @@ public class LmsView {
         int choice;
         Lable1:
         do {
+            System.out.println();
             System.out.println("************* Library Management System ****************");
             System.out.println(" ---------- Login As ------------");
             System.out.println("\t   1. Admin   ");
@@ -29,27 +34,29 @@ public class LmsView {
             choice = sc.nextInt();
 
             if (choice == 1) {
-                String userName;
+                Integer userId;
                 userloop1:
                 do {
-                    System.out.println("Enter your User name : ");
-                    userName = sc.next();
-                    if (userName == null) {
+                    System.out.println("Enter your User Id : ");
+                    userId = sc.nextInt();
+                    if (userId == null) {
                         System.out.println("Invalid User ... Please re enter user name");
                         continue userloop1;
                     }
-                } while (userName == null);
-                if (userName != null) {
-                    System.out.println("Welcome " + userName);
+                } while (userId == null);
+                if (userId != null) {
+                    System.out.println("Welcome " + userId);
                     int choice1 = 1;
                     do {
+                        System.out.println();
                         System.out.println("\t____  Options  ____");
                         System.out.println("\t    1. Add Books ");
                         System.out.println("\t    2. Update Books");
                         System.out.println("\t    3. Delete Books");
                         System.out.println("\t    4. Search Books");
                         System.out.println("\t    5. View Books");
-                        System.out.println("\t    6.Go to Main Menu ");
+                        System.out.println("\t    6. Issue Books");
+                        System.out.println("\t    7.Go to Main Menu ");
                         System.out.println("\t_______________");
                         System.out.println("\tEnter your choice : ");
                         choice1 = sc.nextInt();
@@ -61,11 +68,98 @@ public class LmsView {
                             deleteBooks();
                         } else if (choice1 == 4) {
                             searchBooks();
+                        } else if (choice1 == 6) {
+                            issueBooks();
+                        }
+                    } while (choice1 != 7);
+                }
+            } else if (choice == 2) {
+                Integer userId;
+                userloop1:
+                do {
+                    System.out.println("Enter your User Id : ");
+                    userId = sc.nextInt();
+                    if (userId == null) {
+                        System.out.println("Invalid User ... Please re enter user name");
+                        continue userloop1;
+                    }
+                } while (userId == null);
+                if (userId != null) {
+                    System.out.println("Welcome " + userId);
+                    int choice1 = 1;
+                    do {
+                        System.out.println("\t____  Options  ____");
+                        System.out.println("\t    1. View All Books ");
+                        System.out.println("\t    2. Search Books");
+                        System.out.println("\t    3. Order Books");
+                        System.out.println("\t    4. Return Ordered Books");
+                        System.out.println("\t    5. View Issued Books Information");
+                        System.out.println("\t    6.Go to Main Menu ");
+                        System.out.println("\t_______________");
+                        System.out.println("\tEnter your choice : ");
+                        choice1 = sc.nextInt();
+                        if (choice1 == 1) {
+                            viewAllBooks();
+                        } else if (choice1 == 2) {
+                            searchStudentBooks();
+                        } else if (choice1 == 3) {
+                            orderBooks(userId);
+                        } else if (choice1 == 4) {
+                            returnBooks();
                         }
                     } while (choice1 != 6);
                 }
             }
         } while (choice != 3);
+    }
+
+
+    private void viewAllBooks() {
+        List<Book> b = lmsDao.viewAllBooks();
+
+        for (Book book : b) {
+            System.out.println(book);
+        }
+    }
+
+    private void searchStudentBooks() {
+
+    }
+
+    private void orderBooks(int userid) {
+        List<Book> bookList = lmsDao.viewAllBooks();
+
+        for (Book book : bookList) {
+            System.out.println("\t  " + book.getBookId() + "     " + book.getBookName() + "   " + book.getPublisher() + "    " + book.getQuantity() + "    " + book.getAuthor());
+        }
+        System.out.println("\t Enter book id to be ordered: ");
+        int bid = sc.nextInt();
+
+        lmsDao.orderBooks(userid, bid);
+    }
+
+    private void issueBooks() {
+        System.out.println("Hi");
+        System.out.println(" Enter book id to be issued");
+        int bid = sc.nextInt();
+        System.out.println("Enter userid whose book is to be issued ");
+        int userid = sc.nextInt();
+
+        ArrayList<IssueDetails> pendingIssueRequestsList = lmsDao.pendingIssueRequests();
+        for (IssueDetails issueDetail : pendingIssueRequestsList) {
+            System.out.println("  " + issueDetail.getBookId() + "   " + issueDetail.getUserId());
+        }
+
+
+        lmsDao.issueBook(bid, userid);
+    }
+
+    private void returnBooks() {
+        Book book = new Book();
+        System.out.println("\t Enter book id to return. ");
+        book.setBookId(sc.nextInt());
+
+        lmsDao.returnBooks();
     }
 
     private void addBooks() {
@@ -80,8 +174,8 @@ public class LmsView {
         book.setQuantity(sc.nextInt());
 
         lmsDao.addBooks(book);
-
     }
+
 
     private void updateBooks() {
         Book book = new Book();
@@ -100,7 +194,6 @@ public class LmsView {
     }
 
     private void deleteBooks() {
-
         Book book = new Book();
         System.out.println("\t Enter book id to delete. ");
         book.setBookId(sc.nextInt());
@@ -109,11 +202,11 @@ public class LmsView {
     }
 
     private void searchBooks() {
-
-        Book book = new Book();
         System.out.println("\t Enter book id to search: ");
-        book.setBookId(sc.nextInt());
+        int bookid = sc.nextInt();
 
-        lmsDao.searchBooks(book);
+        Book b = lmsDao.searchBooks(bookid);
+        System.out.println(" \t   " + b.getBookId() + "  \t   " + b.getBookName() + "  \t  " + b.getPublisher() + "  \t  " + b.getQuantity() + "  \t    " + b.getAuthor());
+        System.out.println();
     }
 }
