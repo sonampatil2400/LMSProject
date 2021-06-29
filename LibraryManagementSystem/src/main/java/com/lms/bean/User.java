e96715c0380fa53176c5068d4285d462cb8c26e1
sonampatil2400 <85982582+sonampatil2400@users.noreply.github.com>
2021-06-29 20:19:30 +0530

    Add files via upload
    Updates 29th June

---
+++ LibraryManagementSystem/src/main/java/com/lms/bean/User.java
@@ -0,0 +1,55 @@
+package com.lms.bean;
+
+public class User {
+
+
+    int userId;
+    String userName;
+    String userType;
+    int issuedBookId;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public int getIssuedBookId() {
+        return issuedBookId;
+    }
+
+    public void setIssuedBookId(int issuedBookId) {
+        this.issuedBookId = issuedBookId;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", userName='" + userName + '\'' +
+                ", userType='" + userType + '\'' +
+                ", issuedBookId=" + issuedBookId +
+                '}';
+    }
+
+    public User() {
+    }
+}
